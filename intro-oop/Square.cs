c0a62acde2a97503cc3278ee5685304aef76d2c3
lukesmith24 <wellsvilleluke@gmail.com>
2025-09-08 02:41:07 +0000

    Fix namespace typo and implement Square class with Area method

--- intro-oop/Square.cs
+++ intro-oop/Square.cs
@@ -1,4 +1,18 @@
 namesapce intro_oop
 {
     public class Square : Shape
+    {
+
+        private readonly double _side;
+
+        public Square (double side)
+        {
+            _side = side;
+        }
+
+        public override double Area()
+        {
+            return _side * _side;
+        }
+    }
 }
